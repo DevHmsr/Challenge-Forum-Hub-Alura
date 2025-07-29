5af4b7c63a64304842e40ce140c392fcfb1c527a
Heitor Matos <eng.heitormatos@gmail.com>
2025-07-29 12:34:45 -0300

    primeiro commit

---
+++ src/test/java/br/com/alura/forum/hub/api/ForumHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.forum.hub.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
